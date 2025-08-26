86397d5dfeddfe3536ffc6943c43998eb02aeb88
Spardutti <luisdamian.sp@gmail.com>
2025-08-26 12:03:39 +0000

    fix(animal): null dates

--- src/resources/animal/animal.service.ts
+++ src/resources/animal/animal.service.ts
@@ -69,6 +69,8 @@ export class AnimalService extends BaseService {
 		return this.db.sequelize.transaction(async (transaction) => {
 			const { breedId, speciesId, fatherId, motherId } = await this.decodeAnimalIds(data);
 
+			const { acquisitionDate, birthDate, ...rest } = data;
+
 			if (fatherId || motherId) {
 				await this.validateParents(fatherId, motherId, transaction);
 			}
@@ -76,7 +78,9 @@ export class AnimalService extends BaseService {
 			await this.validateBreedSpeciesMatch(breedId, speciesId, transaction);
 			await this.validateTagNumberUniqueness(data.tagNumber, data.farmId, speciesId, transaction);
 
-			return this.db.models.Animal.create({ ...data, breedId, speciesId, fatherId, motherId, name: data.name || '' }, { transaction });
+			const createData = { ...rest, breedId, speciesId, fatherId, motherId, name: data.name || '', ...(birthDate ? { birthDate } : {}), ...(acquisitionDate ? { acquisitionDate } : {}) };
+
+			return this.db.models.Animal.create(createData, { transaction });
 		});
 	}
 
@@ -109,6 +113,8 @@ export class AnimalService extends BaseService {
 
 			const { breedId, speciesId, fatherId, motherId } = await this.decodeAnimalIds(data);
 
+			const { acquisitionDate, birthDate, ...rest } = data;
+
 			if (fatherId || motherId) {
 				await this.validateParents(fatherId, motherId, transaction);
 			}
@@ -116,14 +122,9 @@ export class AnimalService extends BaseService {
 			await this.validateBreedSpeciesMatch(breedId, speciesId, transaction);
 			await this.validateTagNumberUniqueness(data.tagNumber, farmId, speciesId, transaction);
 
-			await animal.update({
-				...data,
-				breedId,
-				speciesId,
-				fatherId,
-				motherId,
-				farmId,
-			}, { transaction });
+			const createData = { ...rest, breedId, speciesId, fatherId, motherId, name: data.name || '', ...(birthDate ? { birthDate } : {}), ...(acquisitionDate ? { acquisitionDate } : {}) };
+
+			await animal.update(createData, { transaction });
 
 			return animal;
 		});
