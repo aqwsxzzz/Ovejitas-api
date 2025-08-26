dc95bb5643948668f3d9cdaceec5390aba165139
Spardutti <luisdamian.sp@gmail.com>
2025-08-26 12:51:20 +0000

    fix(server): allowed headers

--- src/server.ts
+++ src/server.ts
@@ -44,7 +44,14 @@ server.register(cors, {
 	},
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['*'],
+	allowedHeaders: [
+		'Accept',
+		'Content-Type',
+		'Authorization',
+		'Origin',
+		'X-Requested-With',
+		'Cookie',
+	],
 	exposedHeaders: ['set-cookie'],
 });
 
