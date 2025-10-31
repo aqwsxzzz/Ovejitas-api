f88361c3939e4f014ea60d1b7bed1dc406f38753
Spardutti <luisdamian.sp@gmail.com>
2025-10-31 10:49:10 +0000

    feat: set new db

--- src/database/sequelize-config.js
+++ src/database/sequelize-config.js
@@ -3,42 +3,25 @@ require('dotenv').config();
 
 module.exports = {
 	development: {
-		username: process.env.DB_USER || 'user',
-		password: process.env.DB_PASS || 'password',
-		database: process.env.DB_NAME || 'database',
-		host: process.env.DB_HOST || 'localhost',
-		port: Number(process.env.DB_PORT) || 5432,
-		dialect: process.env.DB_DIALECT || 'postgres',
+		use_env_variable: 'DATABASE_URL',
+		dialect: 'postgres',
 		logging: process.env.NODE_ENV !== 'production',
-		dialectOptions: {
-			ssl: process.env.DB_HOST && process.env.DB_HOST.includes('supabase') ? {
-				require: true,
-				rejectUnauthorized: false,
-			} : false,
-		},
+		dialectOptions: process.env.DATABASE_URL?.includes('supabase')
+			? { ssl: { require: true, rejectUnauthorized: false } }
+			: {},
 	},
 	test: {
+		dialect: 'postgres',
 		username: process.env.DB_USER || 'user',
 		password: process.env.DB_PASS || 'password',
 		database: process.env.DB_NAME || 'database_test',
 		host: process.env.DB_HOST || 'localhost',
-		port: Number(process.env.DB_PORT) || 5432,
-		dialect: process.env.DB_DIALECT || 'postgres',
 		logging: false,
 	},
 	production: {
-		username: process.env.DB_USER || 'user',
-		password: process.env.DB_PASS || 'password',
-		database: process.env.DB_NAME || 'database_prod',
-		host: process.env.DB_HOST || 'localhost',
-		port: Number(process.env.DB_PORT) || 5432,
-		dialect: process.env.DB_DIALECT || 'postgres',
+		use_env_variable: 'DATABASE_URL',
+		dialect: 'postgres',
 		logging: false,
-		dialectOptions: {
-			ssl: {
-				require: true,
-				rejectUnauthorized: false,
-			},
-		},
+		dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 	},
 };
